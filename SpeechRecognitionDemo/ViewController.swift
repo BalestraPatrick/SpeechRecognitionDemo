a078bc3c124a8d368f0f5d8d2ccb4e1831fdfc68
Patrick Balestra <me@patrickbalestra.com>
2017-01-06 11:39:42 +0100

    Add empty Xcode project

---
+++ SpeechRecognitionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechRecognitionDemo
+//
+//  Created by Patrick Balestra on 1/6/17.
+//  Copyright © 2017 Patrick Balestra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
