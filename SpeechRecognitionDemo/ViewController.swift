54d1cb718becdea9f4dabf48a887f3afd21c63ac
Patrick Balestra <me@patrickbalestra.com>
2017-01-14 21:01:41 +0100

    Implement by following the tutorial

--- SpeechRecognitionDemo/ViewController.swift
+++ SpeechRecognitionDemo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
 
@@ -20,15 +21,59 @@ class ViewController: UIViewController {
         }
     }
 
+    var preRecordedAudioURL: URL = {
+        return Bundle.main.url(forResource: "LX40", withExtension: "m4a")!
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         applyStyle()
 
+        switch SFSpeechRecognizer.authorizationStatus() {
+        case .notDetermined:
+            askSpeechPermission()
+        case .authorized:
+            self.status = .ready
+        case .denied, .restricted:
+            self.status = .unavailable
+        }
+
     }
 
-    // TODO
+    func askSpeechPermission() {
+        SFSpeechRecognizer.requestAuthorization { status in
+            OperationQueue.main.addOperation {
+                switch status {
+                case .authorized:
+                    self.status = .ready
+                default:
+                    self.status = .unavailable
+                }
+            }
+        }
+    }
 
-    @IBAction func microphonePressed() {
+    func recognizeFile(url: URL) {
+        guard let recognizer = SFSpeechRecognizer(), recognizer.isAvailable else {
+            return
+        }
 
+        let request = SFSpeechURLRecognitionRequest(url: url)
+        recognizer.recognitionTask(with: request) { result, error in
+            guard let recognizer = SFSpeechRecognizer(), recognizer.isAvailable else {
+                return self.status = .unavailable
+            }
+            if let result = result {
+                self.flightTextView.text = result.bestTranscription.formattedString
+                if result.isFinal {
+                    self.searchFlight(number: result.bestTranscription.formattedString)
+                }
+            } else if let error = error {
+                print(error)
+            }
+        }
+    }
+    @IBAction func microphonePressed(_ sender: Any) {
+        recognizeFile(url: preRecordedAudioURL)
     }
 }
