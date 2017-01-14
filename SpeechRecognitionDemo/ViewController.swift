069b3f8d3905e24814059edb90231bbe636da766
Patrick Balestra <me@patrickbalestra.com>
2017-01-14 20:51:32 +0100

    Implement by following the tutorial

--- SpeechRecognitionDemo/ViewController.swift
+++ SpeechRecognitionDemo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
 
@@ -20,15 +21,84 @@ class ViewController: UIViewController {
         }
     }
 
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         applyStyle()
 
+        switch SFSpeechRecognizer.authorizationStatus() {
+        case .notDetermined:
+            askSpeechPermission()
+        case .authorized:
+            self.status = .ready
+        case .denied, .restricted:
+            self.status = .unavailable
+        }
+
     }
 
-    // TODO
+    func askSpeechPermission() {
+        SFSpeechRecognizer.requestAuthorization { status in
+            OperationQueue.main.addOperation {
+                switch status {
+                case .authorized:
+                    self.status = .ready
+                default:
+                    self.status = .unavailable
+                }
+            }
+        }
+    }
+
+    func startRecording() {
+        // Setup audio engine and speech recognizer
+        guard let node = audioEngine.inputNode else { return }
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+            self.request.append(buffer)
+        }
+
+        // Prepare and start recording
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+            self.status = .recognizing
+        } catch {
+            return print(error)
+        }
+
+        // Analyze the speech
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
+            if let result = result {
+                self.searchFlight(number: result.bestTranscription.formattedString)
+            } else if let error = error {
+                print(error)
+            }
+        })
+    }
+
+    func cancelRecording() {
+        audioEngine.stop()
+        if let node = audioEngine.inputNode {
+            node.removeTap(onBus: 0)
+        }
+        recognitionTask?.cancel()
+    }
 
     @IBAction func microphonePressed(_ sender: Any) {
-
+        switch status {
+        case .ready:
+            startRecording()
+            status = .recognizing
+        case .recognizing:
+            cancelRecording()
+            status = .ready
+        default:
+            break
+        }
     }
 }
